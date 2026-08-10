074af3e4df22cbb75dc000980f3ef5122a531bba
Danna Gabriela Alvarado Perez <danna_3041240405@utd.edu.mx>
2026-08-10 17:02:27 -0600

    PRUEBA FINAL

--- rutas-service/src/buscarruta.route.js
+++ rutas-service/src/buscarruta.route.js
@@ -11,7 +11,7 @@ function distancia(lat1, lng1, lat2, lng2) {
 const R = 6371000;
 const rad = Math.PI / 180;
 
-
+```
 const dLat = (lat2 - lat1) * rad;
 const dLng = (lng2 - lng1) * rad;
 
@@ -26,7 +26,7 @@ return R * 2 * Math.atan2(
     Math.sqrt(a),
     Math.sqrt(1 - a)
 );
-
+```
 
 }
 
@@ -823,7 +823,6 @@ destinoLng
         error: error.message
     });
 }
-
 
 });
 
